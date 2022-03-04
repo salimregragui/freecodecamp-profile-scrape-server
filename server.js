f94f096896afd259c79bada7bb743891a23864a4
Salim Regragui <40745001+salimregragui@users.noreply.github.com>
2022-03-04 14:37:53 +0100

    fixing cors heroku

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ const fs = require("fs");
 const app = express();
 app.use(cors());
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
+
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
